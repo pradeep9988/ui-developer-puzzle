a4df0be7f76b04cd9612f2a327081598f80a0eb6
Pradeep Goduma <pradeepchandra.goduma1@t-mobile.com>
2023-04-23 12:35:37 +0530

    Task 4: Ability to mark a book as finished

--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
+import {
+  getReadingList,
+  removeFromReadingList,
+  toggleFinishedAction,
+} from '@tmo/books/data-access';
+import { ReadingListItem } from '@tmo/shared/models';
 
 @Component({
   selector: 'tmo-reading-list',
   templateUrl: './reading-list.component.html',
-  styleUrls: ['./reading-list.component.scss']
+  styleUrls: ['./reading-list.component.scss'],
 })
 export class ReadingListComponent {
   readingList$ = this.store.select(getReadingList);
@@ -14,5 +19,9 @@ export class ReadingListComponent {
 
   removeFromReadingList(item) {
     this.store.dispatch(removeFromReadingList({ item }));
+  }
+
+  changeToFinished(item: ReadingListItem) {
+    this.store.dispatch(toggleFinishedAction({ item }));
   }
 }
